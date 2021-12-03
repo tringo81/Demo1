959ad1c894dc1d400d41a6e3c4d35d390ca7236c
Tri Ngo <95460077+tringo81@users.noreply.github.com>
2021-12-03 14:03:49 +0700

    add new source code
    commit code

---
+++ Demo1/Demo1/Demo1/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Demo1.ViewModels;
+using Demo1.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Demo1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Demo1/Demo1/Demo1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Demo1.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Demo1.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
